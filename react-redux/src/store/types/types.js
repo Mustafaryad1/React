c345a128e442e43809c2e030a787c8ef97798fae
Mustafa Ryad <mustafariad03@gmail.com>
2021-02-11 00:09:33 +0200

    Delete react-redux directory

--- react-redux/src/store/types/types.js
+++
@@ -1,4 +0,0 @@
-export const CHANGE_LANGUAGE = 'CHANGE_LANGUAGE';
-export const ADD_TO_CART = 'ADD_TO_CART';
-
-export const GET_USERS = 'GET_USERS'
--- react-redux/src/utlis/languageContextConfig.js
+++
@@ -1,8 +0,0 @@
-// Create context file config
-import React from "react";
-const LanguageContext = React.createContext({
-  lang: "en",
-  setLang: () => {},
-});
-export const LanguageProvider= LanguageContext.Provider;
-export default LanguageContext ;
